d12392a9c5b754d1a5651f6b43be5895800be03b
IvanTrofimov <ivantrofimov052@gmail.com>
2019-10-14 18:08:35 +0300

    #case1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import turtle
+
 def Square(x,y,size,color,angel):
     #Do(Гоша)
     print()
@@ -5,5 +7,21 @@ def Triangle(x,y,size,color,angel):
     #Do(Егор)
     print()
 def Parallelogram(x,y,size,color,angel):
-    # Do(Я)
-    print ()
+    turtle.pendown ()
+    turtle.color (color)
+    turtle.penup ()
+    turtle.setx ( x )
+    turtle.sety ( y )
+    turtle.begin_fill ()
+    turtle.pendown ( )
+    turtle.right(angel)
+    turtle.forward (size)
+    turtle.right(60)
+    turtle.forward ( size )
+    turtle.right (120)
+    turtle.forward ( size )
+    turtle.right ( 60 )
+    turtle.forward ( size )
+    turtle.end_fill ()
+Parallelogram(100,100,100,'green',90)
+turtle.mainloop()
