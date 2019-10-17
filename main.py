244eeb9348acd108de6761b22a31c2381058768d
GoshaSalosin <56682096+GoshaSalosin@users.noreply.github.com>
2019-10-17 21:20:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import turtle
 def Square(x,y,size,color,angel):
     #Do(Егор) коммитнулось проверка
     print()
-def Triangle(x,y,size,color,angel):
-    #Do(Гоша)
-    print()
 def Parallelogram(x,y,size,color,angel):
     turtle.pendown ()
     turtle.color (color)
@@ -24,4 +21,22 @@ def Parallelogram(x,y,size,color,angel):
     turtle.forward ( size )
     turtle.end_fill ()
 Parallelogram(100,100,100,'green',90)
+turtle.mainloop()
+#Triangle(Гоша)
+import turtle
+from turtle import *
+def Triangle(x,y,a,b,n,color,col,angel):
+    turtle.shape("turtle")
+    turtle.setx(x)
+    turtle.sety(y)
+    turtle.width(n)
+    turtle.color(color,col)
+    turtle.begin_fill()
+    turtle.pendown()
+    turtle.fd(a)
+    turtle.right(angel)
+    turtle.fd(b)
+    turtle.goto(x,y)
+    turtle.end_fill()
+Triangle(-200,200,300,200,2,"red","blue",90)
 turtle.mainloop()
