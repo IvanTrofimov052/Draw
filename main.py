f3fa76a6cc084315f9bcae1d859fe297414ddc46
student <guskov.platon@gmail.com>
2019-10-25 12:36:08 +0700

    case9 fixbags

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ def Square(x,y,size,color,angel):
     turtle.right (90)
     turtle.forward ( size )
     turtle.end_fill ()
+    if (angel_1 >= 360):
+        angel_1 = angel_1 - 360
+
 def Parallelogram(x,y,size,color,angel):
     global angel_1
     turtle.pendown ()
@@ -41,6 +44,9 @@ def Parallelogram(x,y,size,color,angel):
     turtle.right ( 60 )
     turtle.forward ( size )
     turtle.end_fill ()
+    if (angel_1 >= 360):
+        angel_1 = angel_1 - 360
+
 def Triangle(x,y,size,color,angel):
     global angel_1
     turtle.penup()
@@ -55,17 +61,8 @@ def Triangle(x,y,size,color,angel):
     turtle.right(90)
     turtle.forward(size)
     turtle.end_fill()
+    if (angel_1>=360):
+        angel_1=angel_1-360
 
-if(angel_1>=360):
-    angel_1=angel_1-360
-
-
-Square(-700,-100,100,"green",90)
-Parallelogram(-750,-100,50,"green",30)
-Parallelogram(-650,-15,50,"green",60)
-Triangle(-700,-150,200,"green",240)
-
-Square(-700,200,200,"red",90)
-Triangle(-700,400,200,"green",150)
 
 turtle.mainloop()
