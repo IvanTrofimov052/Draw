87c9449242aa10cf362be56fd3cc304ede2ece6b
EgorKhantakov <babloo5647@gmail.com>
2019-10-25 18:14:49 +0700

    case12 figures after fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import turtle
 
 global angel_1
 angel_1 = 0
-
+turtle.speed(100000)
 def Square(x, y, size, color, angel):
     global angel_1
     turtle.pendown()
@@ -64,6 +64,11 @@ def Triangle(x, y, size, color, angel):
     if (angel_1 >= 360):
         angel_1 = angel_1 - 360
 
-Square(-100, 100, 200, 'green', 0)
+Square(-100, -200, 200, 'green', 0)
+Triangle(-133, -200, 190, 'red', 45)
+Square(-50, -250, 100, 'black', 0)
+
+Parallelogram(-100, 150, 200, '#800000', 75)
+Parallelogram(-25, 195, 100, 'black', -120)
 
 turtle.mainloop()
