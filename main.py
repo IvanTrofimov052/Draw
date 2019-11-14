df91375a272b954fc1a6b803f8f06da71c0d33ab
IvanTrofimov052 <55573197+IvanTrofimov052@users.noreply.github.com>
2019-11-14 19:29:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,10 @@ def Triangle(x, y, size, color, angel):
     turtle.forward(size)
     turtle.end_fill()
 
+#tree
+Square(-600, -200, 30, '#6e5614', 0)
+Triangle(-720, -200, 205, 'green', 45)
+Triangle(-675, -70, 150, 'green', 315)
 
 # Bat
 Square(600, -200, 100, 'black', 0)
@@ -82,5 +86,10 @@ Square(600, 148, 100, 'black', 0)
 Square(500, 148, 100, 'blue', 0)
 Square(700, 148, 100, 'yellow', 0)
 Square(500, 148, 100, 'silver', 270)
+
+#home
+Square(-600, 200, 200, '#6e5614', 0)
+Triangle(-840, 400, 205, 'red', 226)
+Square(-725, 275, 50, 'blue', 1)
 
 turtle.mainloop()
